2bbb5e599aec947a2f6ee3eaa82556b8224a00e2
Joseph Lee <jmleesjc2000@gmail.com>
2021-10-16 15:49:42 -0400

    Added calc

---
+++ calculator.js
@@ -0,0 +1,7 @@
+const readline = require('readline-sync')
+
+console.log('Welcome to the Calculator')
+
+let question = readline.question('What is the first number?')
+console.log(typeof question)
+
